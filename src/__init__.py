88c380c3bcb6d10f8d5613f7b0c8fc956a9aed55
BlakeC97 <cjblake97@gmail.com>
2020-11-01 15:03:38 -0500

    PDF transcription works!

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,19 @@
+from pathlib import Path
+
 from flask import Flask
 from .views import homepage, pdf_ocr
 
 
+ROOT_DIR = Path(__file__).parent
+
+
 def create_app():
     app = Flask(__name__)
+
+    app.config["MAX_CONTENT_LENGTH"] = 15 * 1024 * 1024
+    app.config["UPLOAD_FOLDER"] = ROOT_DIR / "uploads"
+    app.config["UPLOAD_FOLDER"].mkdir(exist_ok=True)
+
     app.register_blueprint(homepage)
     app.register_blueprint(pdf_ocr)
     return app
--- src/views/pdf_ocr.py
+++ src/views/pdf_ocr.py
@@ -1,6 +1,7 @@
-from typing import List
+import pathlib
+from typing import List, Union
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, redirect, request, current_app, send_from_directory
 import numpy as np
 import pdf2image
 import PIL
@@ -8,6 +9,7 @@ from PIL.Image import Image
 import pytesseract
 from skimage.color import rgb2gray
 from skimage.filters import unsharp_mask, threshold_otsu
+from werkzeug.utils import secure_filename
 
 
 pdf_ocr = Blueprint("pdf_ocr", __name__)
@@ -18,14 +20,46 @@ def pdf():
     return render_template("pdf_ocr.html")
 
 
+# TODO: Validate that the user ACTUALLY uploaded a PDF
+@pdf_ocr.route("/pdf", methods=["POST"])
+def upload_file():
+    uploaded_file = request.files["file"]
+    filename = secure_filename(uploaded_file.filename)
+
+    if filename:
+        current_app.config["UPLOAD_FOLDER"].mkdir(exist_ok=True)
+        pdf_path = current_app.config["UPLOAD_FOLDER"] / filename
+        text_file_path = current_app.config["UPLOAD_FOLDER"] / (pathlib.Path(filename).stem + ".txt")
+        if text_file_path.exists():
+            return send_plain_text_file(text_file_path.name)
+
+        uploaded_file.save(pdf_path)
+        text = pdf_to_text(pdf_path)
+        with open(text_file_path, "w", encoding="utf=8") as txt_file:
+            txt_file.write(text)
+
+        pdf_path.unlink(missing_ok=True)
+        return send_plain_text_file(text_file_path.name)
+    return redirect(url_for("pdf_ocr.pdf"))
+
+
 # HERE BE BACKEND STUFF
-def pdf_to_text(pdf_path: str) -> str:
+def send_plain_text_file(filename: str):
+    return send_from_directory(
+        current_app.config["UPLOAD_FOLDER"],
+        filename,
+        mimetype="text/plain",
+        as_attachment=True,
+    )
+
+
+def pdf_to_text(pdf_path: Union[str, pathlib.Path]) -> str:
     """
     Read in a PDF file on the given `pdf_path` and convert it to text via Optical Character Recognition.
     :param pdf_path: A string representing the path to the PDF file to be OCR'd.
     :return: A string representing all pages joined together.
     """
-    images = process_images(pdf2image.convert_from_path(pdf_path, dpi=300, grayscale=True))
+    images = process_images(pdf2image.convert_from_path(pdf_path, dpi=300))
     texts = (
         pytesseract.image_to_string(im, lang="eng", config="--dpi 300 --psm 1") for im in images
     )
